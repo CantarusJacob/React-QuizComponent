af163418ef6bcce04f2bdd7e34bc93c2fd0d2890
Jacob Cairncross <jacob.cairncross@office.cantarus.com>
2020-10-12 14:47:38 +0100

    Fixed display so it only displays quiz end or next question

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,13 +9,10 @@ class Quiz extends Component{
         this.state = {quiz_position: 1};
     }
     render(){
-        const isQuizEnd = true//(this.state.quiz_position -1 === quizData.quiz_questions.length);
-        let components = <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
-        if(isQuizEnd){
-            components = [<QuizEnd />, components]
-        }
+        const isQuizEnd = (this.state.quiz_position -1 === quizData.quiz_questions.length);
+        
         return( 
-            components
+            (isQuizEnd)?<QuizEnd/>:<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
         )
     }
 }
