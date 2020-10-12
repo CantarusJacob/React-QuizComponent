51c164eb7ce6c2aa216b58e91b8f663b0dd997b0
Jacob Cairncross <jacob.cairncross@office.cantarus.com>
2020-10-12 16:14:21 +0100

    Error detection nearly operational

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,17 @@ import React, {Component} from 'react';
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component{
+    constructor(props){
+        super(props);
+        this.setState({incorrectAnswer:false});
+    }
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer){
             this.props.showNextQuestionHandler();
+            this.setState({incorrectAnswer:false});
+        }
+        else{
+            this.setState({incorrectAnswer:true});
         }
     }
     render(){
@@ -19,6 +27,7 @@ class QuizQuestion extends Component{
                 {answerOptionButtons}
             </ul>
             </section>
+            {(this.state.incorrectAnswer)?<p className='error'>Sorry, that's not right</p>:null}
         </main>
         )
     }
