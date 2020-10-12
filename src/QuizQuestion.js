f6426e667de3ef4003dfd6e9a4226896c359c224
Jacob Cairncross <jacob.cairncross@office.cantarus.com>
2020-10-12 16:22:45 +0100

    Added error message if you hit wrong answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,7 +4,7 @@ import QuizQuestionButton from './QuizQuestionButton'
 class QuizQuestion extends Component{
     constructor(props){
         super(props);
-        this.setState({incorrectAnswer:false});
+        this.state = {incorrectAnswer:false};
     }
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer){
