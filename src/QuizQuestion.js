f53f19337c239dbafc198860addb43d590dcb30d
Jacob Cairncross <jacob.cairncross@office.cantarus.com>
2020-10-12 15:58:53 +0100

    Now iterates through all buttons

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,7 +2,13 @@ import React, {Component} from 'react';
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component{
+    handleClick(buttonText){
+        if(buttonText === this.props.quiz_question.answer){
+            this.props.showNextQuestionHandler();
+        }
+    }
     render(){
+        const answerOptionButtons = this.props.quiz_question.answer_options.map((option, index) => <QuizQuestionButton clickHandler={this.handleClick.bind(this)} button_text={option} key={index}/>)
         return(
         <main>
             <section>
@@ -10,7 +16,7 @@ class QuizQuestion extends Component{
             </section>
             <section className="buttons">
             <ul>
-                <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                {answerOptionButtons}
             </ul>
             </section>
         </main>
